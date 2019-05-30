61a81fb319b4a385b6ca6dc24a579c2e3c668877
Dushyant Kumbhat <kumbhat10@gmail.com>
2019-05-30 23:50:41 +0100

    finished checks for all added check formation result only instead of array output

--- five_in_a_row.m
+++ five_in_a_row.m
@@ -1,5 +1,5 @@
 clearvars
-nMarkers    = 37;
+nMarkers    = 41;
 heightSize  = 6;
 widthSize   = 7;
 markerSize  = 1000;
@@ -22,28 +22,18 @@ scatter(colRed,rowRed,'Sizedata',markerSize,'MarkerFaceColor',[1 0 0],'MarkerEdg
 hold on;
 scatter(colBlue,rowBlue,'Sizedata',markerSize,'MarkerFaceColor',[0 0 1],'MarkerEdgeColor',[0 0 1]);
 
-
-%% wcheck
+%% #inRow formation check
 directionMatrix = [1 0;0 1 ;1 1 ;-1 1];  %% [horizontal vertical]
-
-% checkFormation = @(fill,inRow) find(diff([0 find(diff(fill)) length(fill)])>=inRow);
-checkFormation = @(fill) (diff([0 find(diff(fill)) length(fill)]));
+checkFormation = @(fill) ~isempty(find(diff([0 find(diff(fill)) length(fill)])>=inRowWinner, 1));
+% checkFormation = @(fill) (diff([0 find(diff(fill)) length(fill)]));
 
 %horizontal check
 horizontalCheck = arrayfun(@(x) checkFormation(gameMatrix(x,:)),1:heightSize,'UniformOutput',0);
 %Vertical check
 verticalCheck = arrayfun(@(x) checkFormation(gameMatrix(:,x)'),1:widthSize,'UniformOutput',0);
-
 %%right diagnol check
 rowCheckLimit = heightSize + 1 - inRowWinner;
 colCheckLimit = widthSize + 1 - inRowWinner;
 rightDiagnolCheck = arrayfun(@(y,x) checkFormation(arrayfun(@(X,Y) gameMatrix(Y,X),x:x+min(heightSize-y+1,widthSize-x+1)-1,y:y+min(heightSize-y+1,widthSize-x+1)-1)),[rowCheckLimit:-1:1 ones(1,colCheckLimit-1)],[ones(1,rowCheckLimit-1) 1:colCheckLimit],'UniformOutput',0);                                   
-
 %%left diagnol check
-% colCheckLimit = inRowWinner;
 leftDiagnolCheck = arrayfun(@(y,x) checkFormation(arrayfun(@(X,Y) gameMatrix(Y,X),x:-1:x-min(heightSize-y+1,x)+1,y:y+min(heightSize-y+1,x)-1)),[rowCheckLimit:-1:1 ones(1,widthSize - inRowWinner)],[widthSize*ones(1,rowCheckLimit-1) widthSize:-1:inRowWinner],'UniformOutput',0);                                    
-
-
-
-
-
